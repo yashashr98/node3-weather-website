7b4456cafc62048101282b762b43c087cd8e63af
unknown <yashas hr>
2022-06-27 23:28:57 +0530

    updated forecast page

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,17 +1,32 @@
-const request = require('request')
+const request = require("request");
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=62f03a7dd7f615863f8ea3f93231aed7&query=' + latitude + ',' + longitude + '&units=f'
+  const url =
+    "http://api.weatherstack.com/current?access_key=62f03a7dd7f615863f8ea3f93231aed7&query=" +
+    latitude +
+    "," +
+    longitude +
+    "&units=f";
 
-    request({ url: url, json: true }, (error, response) => {
-        if (error) {
-            callback('Unable to connect to weather service!', undefined)
-        } else if (response.body.error) {
-            callback('Unable to find location', undefined)
-        } else {
-            callback(undefined, response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degress out.")
-        }
-    })
-}
+  request({ url: url, json: true }, (error, response) => {
+    if (error) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (response.body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(
+        undefined,
+        response.body.current.weather_descriptions[0] +
+          ". It is currently " +
+          response.body.current.temperature +
+          " degress out. It feels like " +
+          response.body.current.feelslike +
+          " degress out. The humidity is " +
+          response.body.current.humidity +
+          "%."
+      );
+    }
+  });
+};
 
-module.exports = forecast
+module.exports = forecast;
