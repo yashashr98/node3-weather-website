a299df0c2c860a3b79bad380ea49fc9f7d050ce7
unknown <yashas hr>
2022-06-27 19:43:53 +0530

    init commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoieWFzaGFzaHIzODM4MyIsImEiOiJjbDNkeXBzeXkwMnVrM2RvM3VocW0yZjl0In0.upl1cmV8gTU1WiEBccT2Eg&limit=1'
+
+    request({ url: url, json: true }, (error, response) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (response.body.features.length === 0) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, {
+                latitude: response.body.features[0].center[0],
+                longitude: response.body.features[0].center[1],
+                location: response.body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
